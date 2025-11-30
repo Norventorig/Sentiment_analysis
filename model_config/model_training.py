edfa837ed143120d7bbbadc59cefd51daa084681
Norvent <gordlab@mail.ru>
2025-11-30 19:33:33 +1000

    wraps dataset init into function

--- model_config/model_training.py
+++ model_config/model_training.py
@@ -2,11 +2,16 @@ import os
 import pandas as pd
 
 
-if os.path.exists(r"dataset"):
-    dataset = pd.read_csv("dataset")
+def get_dataset() -> pd.DataFrame:
+    if os.path.exists(r"dataset"):
+        dataset = pd.read_csv("dataset")
 
-else:
-    from dataset_preparation.data_prep import create_dataset
+    else:
+        from dataset_preparation.data_prep import create_dataset
 
-    create_dataset()
-    dataset = pd.read_csv("dataset")
+        create_dataset()
+        dataset = pd.read_csv("dataset")
+
+    return dataset
+
+
