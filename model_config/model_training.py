5a050de38ffd573bca9fa4f12e2b2e86e43079ab
Norvent <gordlab@mail.ru>
2025-11-30 21:35:56 +1000

    adds tokenizing

--- model_config/model_training.py
+++ model_config/model_training.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import os
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 dataset_path = PROJECT_ROOT / "Sentiment_Analysis" / "model_config" / "dataset.csv"
@@ -21,7 +23,19 @@ def get_dataset() -> pd.DataFrame:
 
 
 dataset = get_dataset()
+X = dataset["text"].astype(str)
+y = dataset["sentiment"].astype(int)
 
-vectorizer = TfidfVectorizer(max_features=5000)
-X = vectorizer.fit_transform(dataset['text'])
-y = dataset['sentiment']
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+max_words = 20000
+max_len = 20
+
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(X_train)
+
+X_train_seq = tokenizer.texts_to_sequences(X_train)
+X_val_seq = tokenizer.texts_to_sequences(X_val)
+
+X_train_pad = pad_sequences(X_train_seq, maxlen=max_len)
+X_val_pad = pad_sequences(X_val_seq, maxlen=max_len)
