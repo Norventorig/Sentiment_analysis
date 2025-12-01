ff9fd78213d39401e8bb2eb73803b5cfb710417b
Norvent <gordlab@mail.ru>
2025-12-01 18:49:14 +1000

    adds model_handler

---
+++ model/handler/model_handler.py
@@ -0,0 +1,27 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+class ModelHandler:
+    def __init__(self, model, tokenizer, pad_len, class_labels):
+        self._model = model
+        self._tokenizer = tokenizer
+        self.pad_len = pad_len
+        self.class_labels = class_labels
+
+    def _prepare_data(self, x):
+        x_seq = self._tokenizer.texts_to_sequences(x)
+        x_pad_seq = pad_sequences(x_seq, maxlen=self.pad_len)
+
+        return x_pad_seq
+
+    def predict(self, x):
+        try:
+            x = self._prepare_data(x=x)
+            pred = self._model.predict(x).argmax(axis=1)
+            pred = self.class_labels[pred]
+
+        except:
+            print('ВОЗНИКЛА ОШИБКА НА ЭТАПЕ ПРЕДСКАЗАНИЯ')
+
+        else:
+            return pred
