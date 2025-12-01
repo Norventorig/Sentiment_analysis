9529d45803842c711947af8136a5ed02c4221979
Norvent <gordlab@mail.ru>
2025-12-01 19:23:06 +1000

    fixes file path system

--- model/core.py
+++ model/core.py
@@ -1,14 +1,18 @@
+from pathlib import Path
 from model.handler.model_handler import ModelHandler
 from tensorflow.keras.models import load_model
 import pickle
 
 
-class_labels = ["negative", 'neutral', "positive"]
-model = load_model(r"model_config\utils\model.keras")
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+utils_path = PROJECT_ROOT / "model" / "model_config" / "utils"
 
-with open(r"model_config\utils\tokenizer.pkl", 'rb') as f:
+class_labels = ["negative", 'neutral', "positive"]
+model = load_model(utils_path / "model.keras")
+
+with open(utils_path / "tokenizer.pkl", 'rb') as f:
     tokenizer = pickle.load(f)
-with open(r"model_config\utils\config.pkl", 'rb') as f:
+with open(utils_path / "config.pkl", 'rb') as f:
     pad_len = pickle.load(f)['max_len']
 
 
